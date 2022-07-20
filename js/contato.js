86bd309f412c336591df8b9b813f97a27d96d15e
Misael Alisson Ursulino de LIma <91353521+misalima@users.noreply.github.com>
2022-07-20 14:20:02 -0300

    First files commit

---
+++ js/contato.js
@@ -0,0 +1,52 @@
+    const btn = document.querySelector("#enviar");
+    
+    btn.addEventListener("click", function (e) {
+
+        //Resgatar os inputs do formulário;
+        const campoNome = document.querySelector("#nome");
+        const nome = campoNome.value;
+        const campoEmail = document.querySelector("#email");
+        const email = campoEmail.value;
+        const campoMensagem = document.querySelector("#mensagem");
+        const mensagem = campoMensagem.value;
+        
+        //Validar campo 'Nome'
+        if (nome.length < 2) {
+            e.preventDefault();
+            document.querySelector(".erro-nome").classList.add("erro");
+            document.querySelector(".erro-nome").innerHTML = "O nome inserido deve possuir entre 2 e 20 letras."
+            
+        } else {
+            document.querySelector(".erro-nome").classList.remove("erro");
+        }
+        //Validar campo 'Email'
+        var usuario = email.substring(0, email.indexOf("@"));
+        var dominio = email.substring(email.indexOf("@")+1, email.length);
+        if ((usuario.length >=1) &&
+            (dominio.length >=3) &&
+            (usuario.search("@")==-1) &&
+            (dominio.search("@")==-1) &&
+            (usuario.search(" ")==-1) &&
+            (dominio.search(" ")==-1) &&
+            (dominio.search(".")!=-1) &&
+            (dominio.indexOf(".") >=1)&&
+            (dominio.lastIndexOf(".") < dominio.length - 1)) {
+            
+            document.querySelector(".erro-email").classList.remove("erro");
+
+        } else {
+            e.preventDefault();
+            document.querySelector(".erro-email").classList.add("erro");
+            document.querySelector(".erro-email").innerHTML = "Por favor digite um email válido." 
+        }
+
+        //Validar campo 'Mensagem'
+        if (mensagem.length < 1) {
+            e.preventDefault();
+            document.querySelector(".erro-mensagem").classList.add("erro");
+            document.querySelector(".erro-mensagem").innerHTML = "Por favor, digite sua mensagem.";
+         
+        } else {
+            document.querySelector(".erro-mensagem").classList.remove("erro");
+        }
+    })
---
+++ js/index.js
@@ -0,0 +1,53 @@
+//Botao Menu Dropdown
+function ativarMenu() {
+    let menu = document.querySelector("#dropMenu");
+    menu.classList.add("show");
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        let menu = document.querySelector("#dropMenu");
+          if (menu.classList.contains('show')) {}
+            menu.classList.remove('show');
+        }
+}
+      
+//Pop-Up Code
+window.onload = function(event) {
+    setTimeout(() => {
+        let popup = document.querySelector(".popup")
+        popup.classList.add('showPopup')
+    }, 1000)
+
+}
+function closePopup() {
+    popup.classList.remove('showPopup')
+}
+
+
+//Slides Novidades
+
+let tempo = 3000;
+let imagem1 = document.querySelector("#img1");
+let imagem2 = document.querySelector("#img2");
+
+function slide() {
+    imagem1.setAttribute("src", "images/happy.jpg");
+    imagem2.setAttribute("src", "images/kori.jpg");
+}
+function slide2() {
+    imagem1.setAttribute("src", "images/marca-texto.jpg")
+    imagem2.setAttribute("src", "images/lapis.webp")
+}
+function start () {
+    setInterval(() => {
+        slide();
+    }, tempo);
+    setInterval(() => {
+        slide2();
+    }, (tempo*2));
+    
+    
+}
+
+window.addEventListener("load", start);
+
